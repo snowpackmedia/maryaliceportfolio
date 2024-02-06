9186e45e44ca997baf6e506971e52cd5852811d5
qrbriggssnowpack@gmail.com <qrbriggssnowpack@gmail.com>
2024-02-06 00:37:56 -0700

    Updates for 2/5 - Round 2

--- scripts/content-loaders.js
+++ scripts/content-loaders.js
@@ -1,33 +1,47 @@
 let costumes = document.querySelectorAll("#costume-list li")
 let content = document.querySelector("#costume-content")
-const pages = ['miracle.html','woods.html','shrek.html','peter.html','newsies.html','suite.html','chickens.html']
+const pages = ['woods.html', 'shrek.html', 'peter.html', 'miracle.html', 'newsies.html', 'suite.html', 'chickens.html']
 
-function highlight(index){
-    costumes.forEach( (item, j) => {
-        if (j==index){
+function highlight(index) {
+    costumes.forEach((item, j) => {
+        if (j == index) {
             item.classList.add('active')
         }
-        else{
+        else {
             item.classList.remove('active')
         }
     })
 }
 
-async function loadContent(filename){
+async function loadContent(filename) {
     let response = await fetch(`costumes/${filename}`)
-    if (response.ok){
+    if (response.ok) {
         let html = await response.text()
         content.innerHTML = html
     }
-    else{
+    else {
         content.innerHTML = "<h3>Problem loading content</h3>"
         console.log(response.statusText)
     }
+}
+
+function imageListeners() {
+    let images = document.querySelectorAll("#costume-cards div img")
+    images.forEach((image) => {
+        console.log('Added listener to '+image)
+        image.addEventListener('click', () => {
+            alert('CLICKED!')
+            let index = image.src.indexOf('/web/')
+            let filename = image.src.slice(0, index) + image.src.slice(index + 5)
+            showModal(filename)
+        })
+    })
 }
 
 costumes.forEach((listItem, index) => {
     listItem.addEventListener('click', () => {
         highlight(index)
         loadContent(pages[index])
+        // imageListeners()
     })
 })
