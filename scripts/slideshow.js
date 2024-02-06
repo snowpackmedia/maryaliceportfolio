e3fd485e5d664da226a924d3cb68b723fdaa25f4
qrbriggssnowpack@gmail.com <qrbriggssnowpack@gmail.com>
2024-02-05 23:01:12 -0700

    2/5 update. New about page, initial commetary about each show

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -26,3 +26,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function nextSlide(){
+  plusSlides(1)
+  setTimeout(nextSlide, 7000) 
+}
+
+setTimeout(nextSlide, 7000) 
